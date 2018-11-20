b731c6f9393a9d13cfcc56a9e6ef59427a842602
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-11-20 08:57:46 -0800

    pretty-printing for container

--- R/container.R
+++ R/container.R
@@ -15,10 +15,38 @@ as_container <- function (x) {
   structure(x, class = 'container')
 }
 
+#' @param n print that many elements
+#' @inheritDotParams base::print
+#'
 #' @export
 #' @rdname container
-#' @inheritDotParams base::print
-print.container <- function (x, ...) {
-  cat0('<container, ', length(x), ' element(s)>')
+print.container <- function (x, n = 3, ...) {
+  stopifnot(is.numeric(n))
+  n <- max(0, min(n, length(x)))
+
+  if (n > 0) {
+    ccat0(default = 'grey', '# A container of ', length(x), ' element', if(n>1)'s', '\n')
+    lapply(x[seq(n-1)], function(y) { print(y); cat('\n') })
+    print(nth(x, n))
+    if (length(x) > n) {
+      ccat0(default = 'grey', '# ... with ', length(x)-n, ' more element', if(n>1)'s', '\n')
+    }
+  } else {
+    cat0(toString(x), '\n')
+  }
+
   invisible(x)
+}
+
+#' @export
+#' @rdname container
+toString.container <- function (x, ...) {
+  n <- length(x)
+  c <- table(map_chr(x, function(y)first(class(y))))
+  nc <- length(c)
+
+  paste0('<container of ',
+         n, if(n>1 && nc>1)'various ', ' element', if(n != 1)'s',
+         if(n>1 && nc==1)paste0(' of class ', names(c)),
+         '>')
 }
