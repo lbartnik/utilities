d387d76d467a38c326044e2000d521dce463e8c8
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-11-20 15:30:49 -0800

    add colourize

--- R/cat.R
+++ R/cat.R
@@ -1,18 +1,20 @@
 #' Colored cat.
-#' 
+#'
 #' @importFrom stringi stri_paste
+#'
+#' @seealso colourize
 #'
 #' @param ... Strings to be printed; optionally named with color names.
 #' @param sep Separator.
 #' @param default Default color; `"default"` means no color.
-#' 
+#'
 #' @export
 #' @rdname cpaste
 cpaste <- function (..., sep = ' ', default = "default")
 {
   cat_chunk <- function (color, chunk, sep) {
     if (is_empty(chunk)) return('')
-    
+
     if (identical(color, 'default') || identical(color, '')) {
       color <- default
     } else {
@@ -20,23 +22,23 @@ cpaste <- function (..., sep = ' ', default = "default")
     }
     stri_paste(color(chunk), sep, sep = '')
   }
-  
+
   grey_style <- crayon::make_style(grDevices::grey(.6), grey = TRUE)
   grey <- function(...) crayon::style(paste0(...), grey_style)
-  
+
   get_color <- function (color) {
     if (identical(color, "grey")) return(grey)
     get(color, envir = asNamespace("crayon"), inherits = FALSE)
   }
-  
+
   default <- if (identical(default, 'default')) as.character else get_color(default)
-  
+
   chunks <- list(...)
   chunks <- chunks[!map_lgl(chunks, is.null)]
   chunks <- lapply(chunks, stri_paste, collapse = sep)
-  
+
   if (!length(names(chunks))) names(chunks) <- rep("", length(chunks))
-  
+
   chunks <- unlist(Map(cat_chunk, names(chunks), chunks, c(rep(sep, length(chunks)-1), '')))
   stri_paste(chunks, collapse = '')
 }
@@ -68,3 +70,59 @@ cinform <- function (..., sep = ' ', default = "default") inform(cpaste(..., sep
 #' @rdname ccat
 cinform0 <- function (..., default = "default") cinform(..., sep = '', default = default)
 
+
+
+#' Add color to text.
+#'
+#' Transforms the input `text` by adding colors to substrings specified
+#' in `...`.
+#'
+#' @param text character string to amend.
+#' @param ... substrings with colors assigned as names.
+#' @param default default color.
+#'
+#' @return `text` with ANSI control sequences around colorized substrings.
+#'
+#' @importFrom rlang is_character
+#'
+#' @rdname colourize
+#' @export
+#'
+#' @examples
+#' colourize("text with words", yellow = "with")
+#' colourize("longer text with many words", yellow = "text", red = "many")
+colourize <- function (text, ..., default = 'default') {
+  chunks <- list(...)
+  stopifnot(all(map_lgl(chunks, is_character)))
+
+  pieces <- break_text(text, as.character(chunks))
+
+  colourized <- map_chr(pieces, function (piece) {
+    i <- match(piece, chunks, nomatch = 0L)
+    c <- if (identical(i, 0L)) default else names(chunks)[i]
+    cpaste(piece, default = c)
+  })
+
+  paste0(colourized, collapse = '')
+}
+
+#' @rdname colourize
+#' @export
+colorize <- colourize
+
+#' @importFrom stringi stri_split_fixed stri_detect_fixed
+break_text <- function (text, by) {
+  stopifnot(identical(unique(by), by))
+
+  for (split_with in by) {
+    text <- map_chr(text, function (piece) {
+      if (!any(stri_detect_fixed(piece, by))) return(piece)
+      pieces <- unlist(stri_split_fixed(piece, split_with))
+      pieces <- Filter(nchar, pieces)
+      if (!length(pieces)) return(piece)
+      c(map_chr(head(pieces, -1), function(x) c(x, split_with)), tail(pieces, 1))
+    })
+  }
+
+  text
+}
---
+++ tests/testthat/test-cat.R
@@ -0,0 +1,14 @@
+context("cat")
+
+test_that("break text", {
+  expect_length(break_text("text with words", "with"), 3)
+  expect_length(break_text("longer text with many words", c("text", "many")), 5)
+})
+
+test_that("colourize", {
+  input  <- "text with words"
+  output <- colourize(input, yellow = "with")
+
+  # crayon turns colors off in non-interactive runs
+  expect_equal(output, "text \033[33mwith\033[39m words")
+})
